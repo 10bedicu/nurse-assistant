b989352bec488643f1ea9150229af29b87e5007b
Shivank Kacker <kacker.shivank@gmail.com>
2026-02-10 10:21:25 +0530

    updated patient info

--- utils/constants.ts
+++ utils/constants.ts
@@ -16,28 +16,34 @@ export const LLMS = {
   },
 } as const;
 
-export const PATIENT_INFO = `65-year-old man, with a history of COPD coming in with acute onset of breathlessness. He says he has been coughing past 4 days, and his sputum production has increased in quantity. He is feeling breathless and this has worsened over past 2 days. He does not give any history of admissions to a hospital or ICU with this complaint in the past. He is on a nebulised dose of steroids which he takes twice a day. He can perform his daily activities but feels breathless on walking and must stop to take a breath. He was a smoker in the past but quit 2 years ago. He is hypertensive and takes amlodipine once in the morning.
-
-On examination he is moderately built, sitting leaning forward on bed, breathless. Temperature: 99 deg F, no pedal edema.
-
-RR: 26/minute, using accessory muscles. Spo2: 85% on room air, BP: 140/90mmHg, heart rate: 100/min
-
-Respiratory system: bilateral wheezes and crepitations.
-
-ABG: pH: 7.18, PCO2: 82mmHg, PaO2: 58mmHg, Hco3: 30mEq/L
-
-Diagnosis:
-
-Doctor makes diagnosis of acute exacerbation of COPD most likely due to pneumonia
-
-Investigations ordered:
-
-Chest x ray, sputum culture, complete blood count
-
-Admitted to the ICU
-
-Patient is initiated on NIV
-
-Medications: inhaled bronchodilators: salbutamol hourly 2-3 doses
-
-Ipratropium bromide: hourly 2-3 doses`;
+export const PATIENT_INFO = `
+  Known case of COPD
+  acute onset of breathlessness. worsened over past 2 days.
+  coughing past 4 days, and his sputum production has increased.
+  No history of ICU admission
+  He is on a nebulised dose of steroids which he takes twice a day.
+  can perform his daily activities but feels breathless on walking and must stop to take a breath.
+  
+  General Examination:
+  On examination he is moderately built, sitting leaning forward on bed, breathless.
+  no pedal edema.
+  Respiratory system: bilateral wheezes and crepitations.
+  ABG: pH: 7.18, PCO2: 82mmHg, PaO2: 58mmHg, Hco3: 30mEq/L
+  
+  Vitals
+  Temperature: 99 deg F,
+  RR: 26/minute, also note - using accessory muscles.
+  Spo2: 85% on room air,
+  BP: 140/90mmHg,
+  heart rate: 100/min
+  
+  Diagnosis:
+  COPD also note - most likely due to pneumonia
+  Investigations ordered:
+  Chest x ray, sputum culture, complete blood count
+  Additional instructions:
+  Admitted to the ICU
+  Patient is initiated on NIV
+  Medications: inhaled bronchodilators: salbutamol hourly 2-3 doses
+  Ipratropium bromide: hourly 2-3 doses
+`;
